fac514c7313a30bffb1c6d53aecf5bc0a0d314c1
dragonplus8 <125908478+dragonplus8@users.noreply.github.com>
2026-09-08 19:03:36 +0100

    Update bridge.h

--- HostFrameworks/LC32/bridge.h
+++ HostFrameworks/LC32/bridge.h
@@ -113,6 +113,7 @@ BOOL host_hook_getClass(const char *name, Class *outClass);
 // Lets framework bridges add native compatibility entry points after all
 // guest methods have been mirrored but before the class is registered.
 void LC32UIKitPrepareGuestClass(Class cls);
+void LC32CFNetworkPrepareGuestClass(Class cls);
 bool LC32UIKitGetViewDuringGuestLoad(id controller, id *view);
 void LC32UIKitScheduleLegacyOverlayLayout(id object, id addedSubview);
 
